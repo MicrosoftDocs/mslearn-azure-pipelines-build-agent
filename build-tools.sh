035799a3e208bce906c2e2fb20113f58c77a69f2
Thomas Petchel <tpetchel@gmail.com>
2020-06-10 16:13:29 -0700

    Install npm

--- build-tools.sh
+++ build-tools.sh
@@ -9,8 +9,8 @@ fi
 # Add the Node.js PPA so that we can install the latest version
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 
-# Install Node.js (which includes npm) and jq
-apt-get install -y nodejs jq
+# Install Node.js, npm, and jq
+apt-get install -y nodejs npm jq
 
 # Install gulp
 npm install -g gulp
